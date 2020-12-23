5b9dd6cd1039e512d1999769cfd30f5ef7b7aa98
eneskadirisler <61199015+eneskadirisler@users.noreply.github.com>
2020-12-23 21:27:37 +0300

    Add files via upload

---
+++ FinalDers3/src/main/java/tr/edu/medipol/yazilimaraclari/ders9/HesaplamaApp.java
@@ -0,0 +1,34 @@
+package tr.edu.medipol.yazilimaraclari.ders9;
+
+public class HesaplamaApp {
+
+	public static void main(String[] args) {
+		
+		int sayi1 = 2;
+		int sayi2 = 4;
+		System.out.printf("%d + %d = %d", sayi1, sayi2, toplama(sayi1, sayi2));
+	}
+	
+	public static int toplama(int sayi1, int sayi2) {
+		return sayi1 + sayi2;
+	}
+	
+	public static int carpma(int sayi1, int sayi2) {
+		return sayi1 * sayi2;
+	}
+	
+	public static int cikarma(int sayi1, int sayi2) {
+		return sayi1 - sayi2;
+	}
+	
+	public static int bolme(int sayi1, int sayi2) {
+		if (sayi2 == 0) {
+			throw new RuntimeException("Payda sifir olamaz");
+		}
+		return sayi1 / sayi2;
+	}
+	
+	public static int kareAlma(int sayi) {
+		return sayi * sayi;
+	}
+}
